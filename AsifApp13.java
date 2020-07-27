8aa74c45c34fbef02c2c653a39ae76ba943b839f
Your Name <you@example.com>
2020-07-27 17:53:55 -0400

    Second Commit

---
+++ AsifApp13.java
@@ -0,0 +1,7 @@
+public class AsifApp13{
+    public static void main(String[] args){
+        System.out.println("Learning java");
+
+
+    }
+}
---
+++ AsifApp28.java
@@ -0,0 +1,15 @@
+public class AsifApp28{
+
+    public static void main(String[] args){
+
+        System.out.println("I am learning jave from basic")
+        
+        int x = 2;
+        int y = 5;
+        int z = 7;
+
+        int total = x * y * z;
+        
+        System.out.println("Total = " + total);
+    }
+}
